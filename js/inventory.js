bce21a7ff572a9f020cb5bab68e4e53f4df1e178
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2017-12-31 13:12:26 -0700

    Added way of calculating taxes on prices

--- js/inventory.js
+++ js/inventory.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
             "engine": "5.0L",
             "transmission": "Manual",
             "photo_url": "http://kennebell.net/wp-content/uploads/2017/09/01-ford-mustang-2007-gt-kenne-bell-Press-1200-600-1024x512.jpg",
-            "price": "$5,999"
+            "price": 5999
         },
         {
             "year": 2005,
@@ -20,7 +20,7 @@ $(document).ready(function() {
             "engine": "1.8L",
             "transmission": "Automatic",
             "photo_url": "https://i0.wp.com/images01.olx-st.com/ui/54/63/87/44/o_1509559491_0b4a1028dff6b06efaa6caf0b8666bd9.jpg?ssl=1",
-            "price": "$4,500"
+            "price": 4500
         },
         {
             "year": 2015,
@@ -31,7 +31,7 @@ $(document).ready(function() {
             "engine": "5.7L",
             "transmission": "Manual",
             "photo_url": "https://hips.hearstapps.com/roa.h-cdn.co/assets/15/50/1449863161-dodge-charger-hellcat.jpg",
-            "price": "$28,499"
+            "price": 28499
         },
         {
             "year": 2012,
@@ -42,7 +42,7 @@ $(document).ready(function() {
             "engine": "3.6L",
             "transmission": "Automatic",
             "photo_url": "https://images.hgmsites.net/lrg/chevrolet-camaro-zl1-carbon-concept_100368423_l.jpg",
-            "price": "$11,999"
+            "price": 11999
         },
         {
             "year": 2007,
@@ -53,7 +53,7 @@ $(document).ready(function() {
             "engine": "1.7L",
             "transmission": "Automatic",
             "photo_url": "https://hips.hearstapps.com/roa.h-cdn.co/assets/17/07/1024x512/landscape-1487022045-0-5.jpg?resize=1200:*",
-            "price": "$3,599"
+            "price": 3599
         }
     ];
     for (let i = 0; i < list.length; i++) {
@@ -67,6 +67,7 @@ $(document).ready(function() {
             "<span class=\"car-price\">" + list[i].price + "</span></div></div>"
         );
     }
+    $('.search-error').hide();
     $("#car-search").keyup(function() {
         var text = $(this).val();
 
@@ -75,6 +76,17 @@ $(document).ready(function() {
             if ($(this).text().toLowerCase().indexOf("" + text.toLowerCase() + "") != -1) {
                 $(this).parent().parent().show();
             }
-        })
+        });
+        if ($('#main-section > div:visible').length === 1) {
+            $('.search-error').show();
+        } else {
+            $('.search-error').hide();
+        }
+    });
+    $('.car-price').click(function() {
+        var price = parseInt($(this).text());
+        var taxes = (price * .0008).toFixed(2);
+        var total = price + parseFloat(taxes);
+        alert("price: " + price + ", taxes: " + taxes + ", total: " + total);
     })
 });
