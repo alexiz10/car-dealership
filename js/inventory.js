bde426bf8a98e61e04d2fbcb8b1c861985c5c637
Alexiz Hernandez <alexiz_hernandez@yahoo.com>
2017-12-31 03:39:52 -0700

    Created array containing car objects

---
+++ js/inventory.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+    var list = [
+        {
+            "year": 2007,
+            "make": "Ford",
+            "model": "Mustang",
+            "color": "Red",
+            "milage": 124233,
+            "engine": "5.0L",
+            "transmission": "Manual",
+            "photo_url": "http://strongauto.net/wp-content/uploads/images/2007-Ford-Mustang_12038.jpg",
+            "price": 5999
+        },
+        {
+            "year": 2005,
+            "make": "Toyota",
+            "model": "Corolla",
+            "color": "Silver",
+            "milage": 184829,
+            "engine": "1.8L",
+            "transmission": "Automatic",
+            "photo_url": "https://upload.wikimedia.org/wikipedia/commons/9/92/2005_Toyota_Corolla_%28ZZE120%29_sedan_01.jpg",
+            "price": 4500
+        },
+        {
+            "year": 2015,
+            "make": "Dodge",
+            "model": "Charger",
+            "color": "Black",
+            "milage": 21043,
+            "engine": "5.7L",
+            "transmission": "Manual",
+            "photo_url": "https://i.pinimg.com/originals/77/54/a6/7754a65b70e8c53ca7432059da4e89ec.jpg",
+            "price": 28499
+        },
+        {
+            "year": 2012,
+            "make": "Chevrolet",
+            "model": "Camaro",
+            "color": "Red",
+            "milage": 57832,
+            "engine": "3.6L",
+            "transmission": "Automatic",
+            "photo_url": "http://gmauthority.com/blog/wp-content/gallery/2012-chevrolet-camaro-zl1_1/2012-chevrolet-camaro-zl1-1.jpg",
+            "price": 11999
+        },
+        {
+            "year": 2005,
+            "make": "Honda",
+            "model": "Civic",
+            "color": "Silver",
+            "milage": 143288,
+            "engine": "1.7L",
+            "transmission": "Automatic",
+            "photo_url": "https://media.ed.edmunds-media.com/honda/civic/2004/oem/2004_honda_civic_sedan_hybrid_s_oem_1_500.jpg",
+            "price": 3599
+        }
+    ];
+});
